5fae5bdbc63205d32e8696637c5626e4030279dd
Techinick82 <116661366+Techinick82@users.noreply.github.com>
2025-07-11 18:26:13 +0530

    Add files via upload

---
+++ Assignment#3. Task-2- Math Module.py
@@ -0,0 +1,15 @@
+#using math module to perform calculations
+from math import sqrt
+from math import log
+from math import sin
+a= int(input('Enter a number of your choice: '))
+#print('The square root of',a, 'is: ',(sqrt(a)))
+#print('The loge of', a, 'is: ', log(a))
+#print('The Sine of', a, 'is: ', sin(a))
+print('Square Root: ',sqrt(a))
+print('Logarithm: ',log(a))
+print('Sine: ',sin(a))
+print("Thank you")
+
+
+
